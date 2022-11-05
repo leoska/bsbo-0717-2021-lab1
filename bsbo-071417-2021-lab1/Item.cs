2f0096b39b223e1129775f6d08d20db126317aae
Leonid <Leo77551@gmail.com>
2022-11-05 15:44:06 +0300

    first commit
    pointers, stack, bubble sort

---
+++ bsbo-071417-2021-lab1/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bsbo_071417_2021_lab1
+{
+    internal class Item
+    {
+        public int Value { get; set; }
+        public Item Previous { get; set; }
+
+
+        public Item(int value, Item previous)
+        {
+            Value = value;
+            Previous = previous;
+        }
+    }
+}
---
+++ bsbo-071417-2021-lab1/Stack.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bsbo_071417_2021_lab1
+{
+    internal class Stack
+    {
+        public Item tail = null;
+
+
+        public Item Pop()
+        {
+            if (tail == null) { throw new Exception("Stack is empty"); }
+
+            Item tmp = tail;
+            tail = tail.Previous;
+            tmp.Previous = null;
+            return tmp;
+        }
+
+        public void Push(Item item)
+        {
+            if (tail == null)
+            {
+                tail = item;
+                tail.Previous = null;
+               
+            }else
+            {
+                item.Previous = tail;
+                tail = item;
+            }
+        }
+        public int Get(int index)
+        {
+            Stack tmp= new Stack();
+            int counter=0;
+            for (counter=0;counter<index;counter++)
+            {
+                tmp.Push(Pop());
+            }
+            int tmp1 = tail.Value;
+            while(counter!=0)
+            {
+                Push(tmp.Pop());
+                counter--;
+            }
+            return tmp1;
+        }
+        public void Set(int index,int value)
+        {
+            Stack tmp = new Stack();
+            int counter = 0;
+            for (counter = 0; counter < index; counter++)
+            {
+                tmp.Push(Pop());
+            }
+            tail.Value = value;
+            while (counter != 0)
+            {
+                Push(tmp.Pop());
+                counter--;
+            }
+        }
+        public void Print()
+        {
+            Item current = tail;
+            while(current != null)
+            {
+                Console.Write(current.Value+" ");
+                current = current.Previous;
+            }
+        }
+    }
+}
